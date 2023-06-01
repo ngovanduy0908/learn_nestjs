7835306e1e9c7ef0140ab94a16d2b6ebc8f1827e
ngovanduy0908 <ngoduy090801@gmail.com>
2023-06-01 11:21:49 +0700

    Save user to database & hash passwords

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './utils/typeorm';
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env.development' }),
     AuthModule,
     UsersModule,
+    ConfigModule.forRoot({ envFilePath: '.env.development' }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_DB_HOST,
+      port: parseInt(process.env.MYSQL_DB_PORT),
+      username: process.env.MYSQL_DB_USERNAME,
+      password: process.env.MYSQL_DB_PASSWORD,
+      database: process.env.MYSQL_DB_NAME,
+      synchronize: true,
+      entities,
+    }),
   ],
   controllers: [],
   providers: [],
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,10 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { IUserService } from './user';
 import { CreateUserDetails } from 'src/utils/types';
+import { User } from 'src/utils/typeorm';
+import { hashPassword } from 'src/utils/helpers';
 
 @Injectable()
 export class UserService implements IUserService {
-  createUser(userDetails: CreateUserDetails) {
-    console.log('UserService.createUser');
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+  async createUser(userDetails: CreateUserDetails) {
+    const existingUser = await this.userRepository.findOneBy({
+      email: userDetails.email,
+    });
+
+    if (existingUser) {
+      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+    }
+    const password = await hashPassword(userDetails.password);
+    const newUser = this.userRepository.create({ ...userDetails, password });
+    return this.userRepository.save(newUser);
   }
 }
